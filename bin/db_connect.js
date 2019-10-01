42420cd778fca2632a93fdb184e0a22245ea80c5
Pedro Cardoso Web <46902702+PedroCardosoWeb@users.noreply.github.com>
2019-10-01 18:16:07 -0300

    Update db_connect.js

--- bin/db_connect.js
+++ bin/db_connect.js
@@ -8,32 +8,6 @@ SimpleOracleDB.extend(oracledb); //Modify the original oracledb lib
 
 var connection;
 
-// var insere = `INSERT INTO bv_edi_proceda (:num_nf, :sr_nf, :dt_efet_entrega, :sn_efet_entrega, :obs_histor, :cod_entrega) VALUES = ? `;
-// var operacao = QueryCRUD; // implantar condicional p metdodos CRUD
-
-// function carrega(){ //recebe 'operacao' como parâmetro
-//   try {
-//     // Get a non-pooled connection
-//     // connection.execute(`INSERT INTO bv_edi_proceda (:num_nf, :sr_nf, :dt_efet_entrega, :sn_efet_entrega, :obs_histor, :cod_entrega) VALUES = ? `, 
-//     // [objEntrega.num_nf, objEntrega.sr_nf, objEntrega.dt_efet_entrega, objEntrega.sn_efet_entrega, objEntrega.obs_histor, objEntrega.cod_entrega],
-//     connection.execute(
-//     insere, // queryCRUD
-//     objEntrega, // obj inserido
-//     {autoCommit: true}, 
-//     function (err, res) {
-//     if(err) {
-//         console.log("error: ", err);
-//     }
-//         else{
-//             console.log(res.insertId);
-//         }
-//     })
-//   } 
-//   catch (err) {
-//       console.error(err);
-//   } 
-// }
-
 async function run() {
   try{
     connection = await oracledb.getConnection(dbConfig);
